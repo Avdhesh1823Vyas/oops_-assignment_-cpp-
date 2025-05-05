22b853bd51d75dc6f166bcceebb4e5758c158919
Avdhesh vyas <avdhesh1823@gmail.com>
2025-05-06 01:19:16 +0530

    Update Q21.cpp

--- Assignment 1/Practical/Q21.cpp
+++ Assignment 1/Practical/Q21.cpp
@@ -7,12 +7,11 @@ int main() {
     int intValue = 42;
     double doubleValue;
 
-    // Convert int to double using static_cast
+ 
     doubleValue = static_cast<double>(intValue);
     cout << "Integer value: " << intValue << endl;
     cout << "Converted to double using static_cast: " << doubleValue << endl;
-
-    // Convert double back to int using static_cast
+    
     double anotherDouble = 42.987;
     int convertedInt = static_cast<int>(anotherDouble);
     cout << "Double value: " << anotherDouble << endl;
